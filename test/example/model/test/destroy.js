719617ad5085184b5fc485264c6631959f7c9e53
Parker Park <youngyongpark@gmail.com>
2018-01-21 03:55:45 +0900

    after unit tests, close db connections.

--- test/example/model/test/destroy.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  db: (db) => {
-    if (db.engine._conn) {
-      Object.keys(db.engine._conn).forEach((key) => {
-        db.engine._conn[key].destroy();
-      });
-    }
-  }
-};
