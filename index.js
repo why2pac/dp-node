1c4ee6ff43bc7ae6bc6614b125f5d7612d5dd041
Parker Park <youngyongpark@gmail.com>
2018-01-26 00:51:34 +0900

    4 spaces to 2 spaces.

--- index.js
+++ index.js
@@ -40,134 +40,134 @@ const express = require('express');
 |*
 */
 module.exports = (options) => {
-    var app = options ? options.app : null;
-    var config = {};
+  var app = options ? options.app : null;
+  var config = {};
 
-    var defaultVal = (val, defaultVal) => {
-        if (val === undefined) {
-            return defaultVal;
-        }
-
-        return val;
+  var defaultVal = (val, defaultVal) => {
+    if (val === undefined) {
+      return defaultVal;
     }
 
-    config.mode = (options.mode || 'web').toLowerCase();
-    config.debug = defaultVal(options.debug, false);
+    return val;
+  }
 
-    config.cfg = {};
-    config.cfg.apppath = options.apppath;
-    config.cfg.controller = options.apppath + ('/controller' || options.controllerPath);
-    config.cfg.view = options.apppath + ('/view' || options.viewPath);
-    config.cfg.minifyRemoveLineBreakWhitespace = defaultVal(options.minifyRemoveLineBreakWhitespace, true);
-    config.cfg.requestSizeLimit = options.requestSizeLimit || '0.5mb';
-    config.cfg.errorLogging = defaultVal(options.errorLogging, true);
+  config.mode = (options.mode || 'web').toLowerCase();
+  config.debug = defaultVal(options.debug, false);
 
-    var errorHandler = options.error || undefined;
+  config.cfg = {};
+  config.cfg.apppath = options.apppath;
+  config.cfg.controller = options.apppath + ('/controller' || options.controllerPath);
+  config.cfg.view = options.apppath + ('/view' || options.viewPath);
+  config.cfg.minifyRemoveLineBreakWhitespace = defaultVal(options.minifyRemoveLineBreakWhitespace, true);
+  config.cfg.requestSizeLimit = options.requestSizeLimit || '0.5mb';
+  config.cfg.errorLogging = defaultVal(options.errorLogging, true);
 
-    config.handler = {};
-    config.handler.error = errorHandler;
+  var errorHandler = options.error || undefined;
 
-    if (typeof config.handler.error === 'function') {
-      config.handler.error = (controller, error, statusCode) => {
-        return async (() => {
-          return await (errorHandler(controller, error, statusCode));
-        })();
-      }
+  config.handler = {};
+  config.handler.error = errorHandler;
+
+  if (typeof config.handler.error === 'function') {
+    config.handler.error = (controller, error, statusCode) => {
+      return async (() => {
+        return await (errorHandler(controller, error, statusCode));
+      })();
+    }
+  }
+
+  config.cfg.viewHelpers = options.viewHelpers || {};
+
+  if (!app) {
+    app = express();
+  }
+
+  config.app = app;
+
+  if (options.logging) {
+    app.use(require('morgan')('short', {}));
+  }
+
+  if (defaultVal(options.redirectNakedToWWW, false)) {
+    app.use(require('express-naked-redirect')());
+  }
+
+  if (defaultVal(options.compression, true)) {
+    app.use(require('compression')());
+  }
+
+  if (defaultVal(options.enhanceSecurity, true)) {
+    app.use(require('helmet')());
+  }
+
+  if (defaultVal(options.cookieEnabled, true)) {
+    app.use(require('cookie-parser')());
+  }
+
+  if (defaultVal(options.session, {})) {
+    config.cfg.session = options.session || {};
+    config.cfg.session.secret = config.cfg.session.secret || 'dR@9oNp0W@r~NoD2';
+    config.cfg.session.volatility = defaultVal(config.cfg.session.volatility, false);
+    config.cfg.session.ttl = config.cfg.session.ttl || 3600 * 24 * 30;
+  }
+
+  if (defaultVal(options.cookie, {})) {
+    config.cfg.cookie = options.cookie || {};
+    config.cfg.cookie.secret = config.cfg.cookie.secret || 'dR@9oNp0W@r~Co0K2';
+    config.cfg.cookie.volatility = defaultVal(config.cfg.cookie.volatility, false);
+    config.cfg.cookie.ttl = config.cfg.cookie.ttl || 3600 * 24 * 30;
+  }
+
+  if (defaultVal(options.databaseDsn, [])) {
+    config.cfg.databaseDsn = options.databaseDsn;
+  }
+
+  app.use('/dp', express.static(__dirname + '/lib/static'));
+
+  if (options.static) {
+    var paths = options.static;
+
+    if (typeof(paths) != 'object') {
+      paths = [paths]
     }
 
-    config.cfg.viewHelpers = options.viewHelpers || {};
+    paths.forEach((e) => {
+      app.use(express.static(options.apppath + '/' + e))
+    })
+  }
 
-    if (!app) {
-        app = express();
-    }
-
-    config.app = app;
-
+  const listen = (port) => {
     if (options.logging) {
-        app.use(require('morgan')('short', {}));
+      console.log('Listening .. ' + port);
     }
 
-    if (defaultVal(options.redirectNakedToWWW, false)) {
-        app.use(require('express-naked-redirect')());
-    }
+    return app.listen(port);
+  }
 
-    if (defaultVal(options.compression, true)) {
-        app.use(require('compression')());
-    }
+  if (options.port && config.mode !== 'job') {
+    listen(options.port);
+  }
 
-    if (defaultVal(options.enhanceSecurity, true)) {
-        app.use(require('helmet')());
-    }
+  config.view = require('./lib/view')(config);
+  config.router = require('./lib/router')(config);
+  config.helper = require('./lib/helper')(config);
+  config.model = require('./lib/model')(config);
 
-    if (defaultVal(options.cookieEnabled, true)) {
-        app.use(require('cookie-parser')());
-    }
+  var dp = {
+    app: app,
+    listen: listen
+  };
 
-    if (defaultVal(options.session, {})) {
-        config.cfg.session = options.session || {};
-        config.cfg.session.secret = config.cfg.session.secret || 'dR@9oNp0W@r~NoD2';
-        config.cfg.session.volatility = defaultVal(config.cfg.session.volatility, false);
-        config.cfg.session.ttl = config.cfg.session.ttl || 3600*24*30;
-    }
+  // Assign config when test mode enabled.
+  if (global.isTest) {
+    dp.config = config;
+  }
 
-    if (defaultVal(options.cookie, {})) {
-        config.cfg.cookie = options.cookie || {};
-        config.cfg.cookie.secret = config.cfg.cookie.secret || 'dR@9oNp0W@r~Co0K2';
-        config.cfg.cookie.volatility = defaultVal(config.cfg.cookie.volatility, false);
-        config.cfg.cookie.ttl = config.cfg.cookie.ttl || 3600*24*30;
-    }
+  // for Job mode.
+  if (config.mode === 'job') {
+    return require('./lib/job')(config);
+  }
 
-    if (defaultVal(options.databaseDsn, [])) {
-        config.cfg.databaseDsn = options.databaseDsn;
-    }
-
-    app.use('/dp', express.static(__dirname + '/lib/static'));
-
-    if (options.static) {
-        var paths = options.static;
-
-        if (typeof(paths) != 'object') {
-            paths = [paths]
-        }
-
-        paths.forEach((e) => {
-            app.use(express.static(options.apppath + '/' + e))
-        })
-    }
-
-    const listen = (port) => {
-        if (options.logging) {
-            console.log('Listening .. ' + port);
-        }
-
-        return app.listen(port);
-    }
-
-    if (options.port && config.mode !== 'job') {
-        listen(options.port);
-    }
-
-    config.view = require('./lib/view')(config);
-    config.router = require('./lib/router')(config);
-    config.helper = require('./lib/helper')(config);
-    config.model = require('./lib/model')(config);
-
-    var dp = {
-        app: app,
-        listen: listen
-    };
-
-    // Assign config when test mode enabled.
-    if (global.isTest) {
-      dp.config = config;
-    }
-
-    // for Job mode.
-    if (config.mode === 'job') {
-      return require('./lib/job')(config);
-    }
-
-    return dp;
+  return dp;
 }
 
 module.exports.Tester = require('./lib/tester');
