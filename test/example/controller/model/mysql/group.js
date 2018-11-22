8c2f700b7d832cb29bc61848139f09713739e28d
GONZO <youngyongpark@gmail.com>
2018-11-22 15:59:19 +0900

    Bind `this` to controller methods.

--- test/example/controller/model/mysql/group.js
+++ test/example/controller/model/mysql/group.js
@@ -1,6 +1,6 @@
 module.exports = {
-  get: async (controller) => {
-    await controller.model.test.group.test();
+  async get() {
+    await this.model.test.group.test();
 
     return 'done';
   },
--- test/example/controller/model/mysql/simple.js
+++ test/example/controller/model/mysql/simple.js
@@ -1,17 +1,17 @@
 const assert = require('assert');
 
 module.exports = {
-  get: async (controller) => {
-    var create = await controller.model.test.createTable() // eslint-disable-line
-    const insert = await controller.model.test.insertRecord();
-    const inserted = await controller.model.test.inquiryRecord();
-    var created2 = await controller.model.test.knex.createTable() // eslint-disable-line
+  async get() {
+    var create = await this.model.test.createTable() // eslint-disable-line
+    const insert = await this.model.test.insertRecord();
+    const inserted = await this.model.test.inquiryRecord();
+    var created2 = await this.model.test.knex.createTable() // eslint-disable-line
 
     assert(insert.insertId);
     assert(inserted.id && inserted.value);
 
-    await controller.model.test.options.test();
-    await controller.model.test.knex.test();
+    await this.model.test.options.test();
+    await this.model.test.knex.test();
 
     return 'done';
   },
