c9ca6ee560ad6569705c8fac84a661487e525141
GONZO <youngyongpark@gmail.com>
2018-12-06 17:50:28 +0900

    __(double underscore) indicates parent module.

--- test/example/controller/model/prop.js
+++ test/example/controller/model/prop.js
@@ -6,6 +6,7 @@ module.exports = {
     assert(this.model.data.foo.baz() === 'baz');
     assert(this.model.data.foo.boz() === 'boz');
     assert(this.model.test.underscoreCalc(1, 2) === this.model.test.underscore.calc(1, 2));
+    assert(this.model.test.underscore.dblUnderscore(1, 2) === this.model.test.underscoreCalc(1, 2));
 
     return 'done';
   },
--- test/example/model/test/underscore.js
+++ test/example/model/test/underscore.js
@@ -2,4 +2,7 @@ module.exports = {
   calc(a, b) {
     return a + b;
   },
+  dblUnderscore(a, b) {
+    return this.__.underscoreCalc(a, b);
+  },
 };
