71233a19729a1a4da1ab80b7f48b7c530aa92ae9
GONZO <youngyongpark@gmail.com>
2020-08-11 10:48:04 +0900

    Update dependencies (#15)
    * Update dependencies
    
    * Update dpendency

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,41 +1,41 @@
 module.exports = {
-  "extends": "airbnb-base",
-  "parserOptions": {
-    "sourceType": "script"
+  extends: 'airbnb-base',
+  parserOptions: {
+    sourceType: 'script',
   },
-  "env": {
-    "jest": true
+  env: {
+    jest: true,
   },
-  "rules": {
-    "global-require": "off",
-    "import/no-dynamic-require": "off",
-    "comma-dangle": ["error", {
-      "arrays": "always-multiline",
-      "objects": "always-multiline",
-      "imports": "always-multiline",
-      "exports": "always-multiline",
-      "functions": "never"
+  rules: {
+    'global-require': 'off',
+    'import/no-dynamic-require': 'off',
+    'comma-dangle': ['error', {
+      arrays: 'always-multiline',
+      objects: 'always-multiline',
+      imports: 'always-multiline',
+      exports: 'always-multiline',
+      functions: 'never',
     }],
-    "no-console": "off",
-    "new-cap": ["error", {
-      "newIsCap": true,
-      "newIsCapExceptions": [
-        "dpError",
-        "dpJobError"
+    'no-console': 'off',
+    'new-cap': ['error', {
+      newIsCap: true,
+      newIsCapExceptions: [
+        'dpError',
+        'dpJobError',
       ],
-      "capIsNew": false,
-      "capIsNewExceptions": [
-        "Immutable.Map",
-        "Immutable.Set",
-        "Immutable.List"
-      ]
+      capIsNew: false,
+      capIsNewExceptions: [
+        'Immutable.Map',
+        'Immutable.Set',
+        'Immutable.List',
+      ],
     }],
-    "no-underscore-dangle": "off",
-    "no-unused-vars": ["error", {
-      "vars": "all",
-      "args": "after-used",
-      "ignoreRestSiblings": true,
-      "argsIgnorePattern": "^_"
-    }]
-  }
+    'no-underscore-dangle': 'off',
+    'no-unused-vars': ['error', {
+      vars: 'all',
+      args: 'after-used',
+      ignoreRestSiblings: true,
+      argsIgnorePattern: '^_',
+    }],
+  },
 };
